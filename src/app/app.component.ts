2892164b9910f7ed60421f7e7cae5ac6795cffe8
Linus Wieland <linus@wieland.click>
2024-07-31 17:41:09 +0200

    &fit=cover&w=50&h=50&position=left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import {NgOptimizedImage} from "@angular/common";
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NgOptimizedImage],
-  template: '<img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png" width="400" height="200" priority alt="">',
+  template: `
+    <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png" width="400" height="200" priority alt=""> +
+    <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png&fit=cover&w=50&h=50&position=left" width="400" height="200" priority alt="">
+  `
+,
   styles: ''
 })
 export class AppComponent {
