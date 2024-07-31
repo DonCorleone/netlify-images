325936e94aa794ff2c334dd8465a7a8a9d441aba
Linus Wieland <linus@wieland.click>
2024-07-31 18:00:23 +0200

    url in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,17 @@ import {NgOptimizedImage} from "@angular/common";
   standalone: true,
   imports: [RouterOutlet, NgOptimizedImage],
   template: `
-    <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png" width="400" height="200" priority alt="">
     <div style="width: 50px; height: 50px">
-      <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png&fit=cover&w=50&h=50&position=left" fill priority alt="" style="max-width: 50px; max-height: 50px" >
+      <img [ngSrc]="imgUrlFixedWithParams" fill priority alt="" style="max-width: 50px; max-height: 50px" >
+    </div>
+    <div>
+      <img [ngSrc]="imgUrlFixed" width="400" height="200" priority alt="">
     </div>
    `
 ,
   styles: ''
 })
 export class AppComponent {
-  title = 'netlify-images';
+  imgUrlFixed = '/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png';
+  imgUrlFixedWithParams = '/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png&fit=cover&w=50&h=50&position=left';
 }
