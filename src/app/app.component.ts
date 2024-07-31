0d4682e1e77a14c389cc1b0a5efb621d8e5ea387
Linus Wieland <linus@wieland.click>
2024-07-31 17:53:13 +0200

    maxwidth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {NgOptimizedImage} from "@angular/common";
   template: `
     <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png" width="400" height="200" priority alt="">
     <div style="width: 50px; height: 50px">
-      <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png&fit=cover&w=50&h=50&position=left" fill priority alt="">
+      <img ngSrc="/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png&fit=cover&w=50&h=50&position=left" fill priority alt="" style="max-width: 50px; max-height: 50px" >
     </div>
    `
 ,
