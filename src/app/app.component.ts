af0019e98ee629fac88b87920da187d465a12026
Linus Wieland <linus@wieland.click>
2024-07-31 17:31:05 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NgOptimizedImage],
+  template: '<img ngSrc="/.netlify/images?url=/../assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png" width="400" height="200" priority alt="">',
+  styles: ''
 })
 export class AppComponent {
   title = 'netlify-images';
