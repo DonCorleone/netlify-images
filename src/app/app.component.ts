899431f0972db2e67c2935fc91f92f4ded86d2d2
Linus Wieland <linus@wieland.click>
2024-09-02 21:58:53 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-
+import * as NetlifyIdentityWidget from "netlify-identity-widget"
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,11 +12,30 @@ import {NgOptimizedImage} from "@angular/common";
     <div>
       <img [ngSrc]="imgUrlFixed" width="400" height="200" priority alt="">
     </div>
+    <div id="netlify-modal">
+    </div>
   `
   ,
   styles: ''
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   imgUrlFixedWithParams = 'https://netlify-images.netlify.app/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png?width=50&height=50';
   imgUrlFixed = 'https://netlify-images.netlify.app/.netlify/images?url=assets/c835453375186b82045edd07b531584660ad0574-2400x1260.png';
+
+
+  ngOnInit() {
+    NetlifyIdentityWidget.init({
+      container: '#netlify-modal', // defaults to document.body
+      locale: 'en' // defaults to 'en'
+    });
+
+    NetlifyIdentityWidget.open(); // open the modal
+
+    NetlifyIdentityWidget.on('init', user => console.log('init', user));
+    NetlifyIdentityWidget.on('login', user => console.log('login', user));
+    NetlifyIdentityWidget.on('logout', () => console.log('Logged out'));
+    NetlifyIdentityWidget.on('error', err => console.error('Error', err));
+    NetlifyIdentityWidget.on('open', () => console.log('Widget opened'));
+    NetlifyIdentityWidget.on('close', () => console.log('Widget closed'));
+  }
 }
