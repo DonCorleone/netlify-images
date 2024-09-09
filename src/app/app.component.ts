a9465a5737a2c09978f196a6bb7215d4e036e32a
Linus Wieland <linus@wieland.click>
2024-09-09 21:19:12 +0200

    ng demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,79 @@ import {RouterLink, RouterOutlet} from "@angular/router";
   standalone: true,
   imports: [NgOptimizedImage, AuthButtonComponent, RouterLink, RouterOutlet],
   template: `
-    <app-auth-button></app-auth-button>
+    <nav>
+      <ul>
+        <li>
+          <div class="login" data-netlify-identity-button></div>
+        </li>
+      </ul>
+    </nav>
+    <div class="content">
+      <h1 class="title">Angular Netlify Identity Demo</h1>
+      <iframe
+        src="https://giphy.com/embed/IbHargNoRiQvmnGyj7"
+        width="300px"
+        height="250px"
+        frameBorder="0"
+      ></iframe>
+      <div class="text">
+        <p>
+          Welcome to the Angular Netlify Identity Demo, glad to have you here! You
+          can find the
+          <a href="https://github.com/tzmanics/angular-netlify-identity-demo"
+          >repo for this project here!</a
+          >
+        </p>
+        <p>
+          (つ・▽・)つ⊂(・▽・⊂)
+        </p>
+
+        <p>
+          You can learn more about
+          <a
+            href="https://docs.netlify.com/visitor-access/identity/?utm_source=blog&utm_medium=angular-identity-tzm&utm_campaign=devex"
+          >Netlify Identity</a
+          >
+          and Angular in
+          <a
+            href="https://www.netlify.app/blog/2020/07/02/authenticating-users-with-angular-and-netlify-identity/?utm_source=blog&utm_medium=angular-identity-tzm&utm_campaign=devex"
+          >this blog post</a
+          >
+          ヽ(´∇｀)ﾉ
+        </p>
+      </div>
+    </div>
+    <router-outlet></router-outlet>
+    <!--app-auth-button></app-auth-button>
     <nav>
       <a routerLink="/public">Public Page</a>
       <a routerLink="/protected">Protected Page</a>
     </nav>
-    <router-outlet></router-outlet>
+    <router-outlet></router-outlet-->
   `
   ,
-  styles: ''
+  styles: `
+    .content {
+      font-size: 20px;
+      margin: 0 auto;
+      max-width: 800px;
+      width: 80%;
+    }
+    .gif {
+      position: absolute;
+      top: 5px;
+      left: 300px;
+    }
+    .title {
+      float: left;
+      margin: 0;
+      width: 200px;
+    }
+    .text {
+      margin: 0 auto;
+      width: 70%;
+    }
+  `
 })
 export class AppComponent {
 }
